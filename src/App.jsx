95f931469b6baee991a1608ab85a8601ac64bb8a
HANSANDINI RU <130740061+HANSANANDINI@users.noreply.github.com>
2024-03-20 11:57:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import Products from './Products';
+
+
+function App() {
+  return (
+    <><div className='Products'>
+      <header><li>productname:laptop1;
+                    price:2234;
+                    rating: 4.5;
+                    discount : 63;
+                    avaiblity:yes
+                    </li></header>
+                    
+
+    </div><Products /></>
+
+
+     
+  ) ;
+}
+
+export default App;
---
+++ src/Products.jsx
@@ -0,0 +1,28 @@
+function Products (){
+    <div className="Products">
+        <header className="Products-Header">
+            
+                <li>productname:laptop1;
+                    price:2234;
+                    rating: 4.5;
+                    discount : 63;
+                    avaiblity:yes
+                    </li>
+                
+            
+            <a
+            className="Product"
+            href="https://wallpapercave.com/wp/wp5485213.jpg">
+            product 1   
+                
+            </a>
+            
+        </header>
+        
+        
+
+    </div>
+    
+}
+
+export default Products;
